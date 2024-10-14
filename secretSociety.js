00bd15636c809b43a9d412086406f6af8729cfbb
CarlosPena <carlos_pena@outlook.de>
2024-10-14 14:33:37 -0600

    A05 | Mejorado el codigo de la función, cubriendo más caso de uso

--- secretSociety.js
+++ secretSociety.js
@@ -1,13 +1,18 @@
 function secretName(names)
 {
    let arN = Array.from(names);
-   return arN = arN.map((cur, i, arr) => (typeof cur === "string" && cur.length > 0) ? cur[0] : "" ).sort().join('');
+   return arN = arN.map((cur, i, arr) => (typeof cur === "string" && cur.length > 0) 
+      ? cur[0].toUpperCase() : "" ).sort().join('');
 }
 
 let result1 = secretName(["Esperanza","Franco", "Nia"]);
 let result2 = secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel']);
 let result3 = secretName(['Harry', 'Ron', 'Hermione']);
+let result4 = secretName(["chaco","ariel",12, "Nia", "Naomi", 0]);
+let result5 = secretName(["chaco","",12, "Nia", "Naomi", 0]);
 
 console.log(result1);
 console.log(result2);
 console.log(result3);
+console.log(result4);
+console.log(result5);
