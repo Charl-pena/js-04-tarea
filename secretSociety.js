cae7917a740b23a61455c6b4bbf8eec9868912dc
CarlosPena <carlos_pena@outlook.de>
2024-10-14 14:49:52 -0600

    A06 | Función mejorada y añadida una version comentada usando reduce

--- secretSociety.js
+++ secretSociety.js
@@ -1,10 +1,14 @@
 function secretName(names)
 {
    let arN = Array.from(names);
-   return arN = arN.map((cur, i, arr) => (typeof cur === "string" && cur.length > 0) 
+   /* 
+      //Version con reduce 
+   return arN.reduce((acc, cur) => (typeof cur === "string" && cur.length > 0)
+      ? acc + cur[0].toUpperCase() : acc , '').split('').sort().join('');
+   */
+   return arN.map((cur, i, arr) => (typeof cur === "string" && cur.length > 0) 
       ? cur[0].toUpperCase() : "" ).sort().join('');
 }
-
 let result1 = secretName(["Esperanza","Franco", "Nia"]);
 let result2 = secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel']);
 let result3 = secretName(['Harry', 'Ron', 'Hermione']);
